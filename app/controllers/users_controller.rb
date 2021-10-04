0ab45f8780c8143f0ce616185f9d2c8ba82d320c
yyuu2479 <yyuu2479@gmail.com>
2021-10-04 05:23:44 +0000

    [Add]user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,28 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
-  
+
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :image)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(first_name: 'yuu', last_name: 'yama', nickname: 'yamayuu', email: 'yyuu@gmail.com', password: '123456')
+10.times{|i| User.create(first_name: "yuu#{i}", last_name: "yama#{i}", nickname: "yamayuu#{i}", email: "yyuu#{i}@gmail.com", password: "123456#{i}")}
