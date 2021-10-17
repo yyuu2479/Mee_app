55b2c5fc303ef9b658d8f38d910919a83dd92ca8
yyuu2479 <yyuu2479@gmail.com>
2021-10-17 07:44:22 +0000

    [Fix]image

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -4,3 +4,5 @@
 #   http_host: 'example.org',
 #   https: false
 # )
+
+Refile.secret_key = 'b22543e580648883408cc106a062867b41eced3d2e0a891c2a011bbcdf9bca3a2f06cd9ad2ae9d04fda6a87fc3881298806e1c366435e7e1488607cb45386255'
